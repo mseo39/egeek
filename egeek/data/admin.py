71516f07951e1f0fb98b3c59d5f7e45bd1d426ef
mseo39 <a01063294905@likelion.org>
2021-07-26 20:14:49 +0900

    model update

--- egeek/data/admin.py
+++ egeek/data/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from data.models import Uploadfile, dorm1_data, dorm2_data, dorm3_data, manager
+from data.models import Uploadfile, dorm1_data, dorm2_data, dorm3_data, manager, old_dorm1_data,old_dorm2_data,old_dorm3_data,global_dorm_data
 # Register your models here.
 
 admin.site.register(Uploadfile)
 admin.site.register(dorm1_data)
 admin.site.register(dorm2_data)
 admin.site.register(dorm3_data)
+admin.site.register(old_dorm1_data)
+admin.site.register(old_dorm2_data)
+admin.site.register(old_dorm3_data)
+admin.site.register(global_dorm_data)
 admin.site.register(manager)
