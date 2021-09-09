7f8ef2ae138908784b4f669d0a2de28f6f3a67fd
mseo39 <a01063294905@gmail.com>
2021-09-09 09:46:36 +0900

    test

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-from datetime import datetime
-import calendar
-
-days=[]
-c= calendar.TextCalendar(calendar.SUNDAY)
-
-for i in c.itermonthdays(datetime.today().year,datetime.today().month):
-    if i==0:
-        days.append([" ","disable"])
-    else:
-        days.append([i," "])
-print(days)
+print("테스트입니다")
